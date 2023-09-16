df448d89659c9239b40b0310f24efb2985f701c0
DieArrC <castanedadiego12.dac@gmail.com>
2023-09-15 20:17:53 -0600

    Completed BetterLinearSearch

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -4,7 +4,14 @@ public static class BetterLinearSearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
+        for (int i = 0; i< data.Count; i++){
+            // Check if the data at the specific index is the target value
+            if (data[i].CompareTo(target) == 0){
+                // Return the index
+                return i;
+            }
+        }
+        // No valid values were found in the list, so return -1 to indicate target not found
         return -1;
     }
 }
