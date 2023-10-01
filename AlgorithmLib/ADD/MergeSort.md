82a7274edca2cc6a8dcb034bc90bd90756d32e84
DieArrC <castanedadiego12.dac@gmail.com>
2023-09-30 22:08:12 -0600

    Completed MergeSort

--- AlgorithmLib/ADD/MergeSort.md
+++ AlgorithmLib/ADD/MergeSort.md
@@ -1,24 +1,55 @@
 # Algorithm Description Document
 
-Author: 
+Author: Diego Arroyo Castaneda
 
-Date: 
+Date: 9/30/23
 
 ## 1. Name
+
 Merge Sort
 
 ## 2. Abstract
 
+A sorting algorithm that uses the concept of divide and conquer to break apart and rebuild a list in order.
+
 ## 3. Methodology
+
+Merge Sort is a particular type of sorting algorithm that uses recursion to split a list into continuously smaller sublists. These sublists are then compared to each other, iterating through their values. Based on the comparison requirements, the lesser (or greater) value are placed, in order, in the original list, and the original list is updated through each recursion. This keeps occuring until all values have been visited, and the list has been sorted.
 
 ## 4. Pseudocode
 
 ```
 MERGE(data, first, mid, last)
 
+IF first > last:
+    RETURN
+LET n1 = mid - first + 1
+LET n2 = last - mid
+
+LET left_data = [], length(n1)
+LET left_data = [], length(n2)
+
+SET left_data = data[0:mid]
+SET right_data = data[mid + 1:last]
+
+LET i and j = 0
+
+FOR k=first to last:
+    IF left_data[i] <= right_data[j]:
+        SET data[k] = left_data[i]
+        INCREMENT i + 1
+    ELSE:
+        SET data[k] = right_data[j]
+        INCREMENT j + 1
 
 SORT(data, first, last)
 
+IF first >= last:
+    RETURN
+LET mid = (first + last) / 2, rounded down
+call SORT(data,first,mid)
+call SORT(data,mid + 1,last)
+call MERGE(data, first mid, last)
 ```
 
 ## 5. Inputs & Outputs
@@ -27,11 +58,15 @@ List only inputs and outputs for the SORT function.
 
 Inputs:
 
+* data - a list of sortable values
+* first - an integer representing the first index of a list
+* last - an integer representing the last index of a list
+
 Outputs:
 
 ## 6. Analysis Results
 
-* Worst Case: $O(?)$
+* Worst Case: $O(nlogn)$
 
-* Best Case: $\Omega(?)$
+* Best Case: $\Omega(nlogn)$
 
--- AlgorithmLib/MergeSort.cs
+++ AlgorithmLib/MergeSort.cs
@@ -1,15 +1,59 @@
-﻿namespace AlgorithmLib;
+﻿using System.Security.AccessControl;
+
+namespace AlgorithmLib;
 
 public static class MergeSort
 {
     private static void Merge(List<IComparable> data, int first, int mid, int last)
     {
-        // ADD CODE HERE 
+        // Split the current list in half into two sublists, with the first half including the midpoint of the list.
+        List<IComparable> first_half = data.GetRange(first,mid - first + 1);
+        List<IComparable> second_half = data.GetRange(mid + 1,last - mid);
+        
+        // Initialize two temporary index handlers, one for each sublist, for iterations.
+        var (first_half_index, second_half_index) = (0,0);
+        
+        // Iterate through the main list from first to last and update the positions of values based on the sublists.
+        for (int i = first; i <= last; i++)
+        {
+            // The first sublist still contains data.
+            if (first_half_index < first_half.Count &&
+            // The second list is empty or iterated through, or the first item in sublist one is not greater than in sublist two.
+            (second_half_index >= second_half.Count||first_half[first_half_index].CompareTo(second_half[second_half_index]) <=0))
+            {
+                data[i] = first_half[first_half_index];
+                first_half_index ++;
+            }
+            // The first sublist is empty/iterated through, or the value in sublist two was lesser.
+            else
+            {
+                data[i] = second_half[second_half_index];
+                second_half_index ++;
+            }
+        }
+
     }
 
     public static void Sort(List<IComparable> data)
     {
-        // ADD CODE HERE 
+        // Call a helper function using default values of index 0 for "first" and the length of the list for "last"
+        _Sort(data,0,data.Count-1);
+    }
+    // Helper function for Sort function
+    public static void _Sort(List<IComparable> data, int first, int last)
+    {
+        // There is at most 1 item in the list
+        if (first >= last)
+        {
+            return;
+        }
+        int mid = (first + last) / 2;
+        // Recursively call the sort function on the first half of the list
+        _Sort(data,first,mid);
+        // Recursively call the sort function on the second half of the list
+        _Sort(data,mid + 1,last);
+        // Call the Merge function on the current sublist
+        Merge(data,first,mid,last);
     }
     
 }
